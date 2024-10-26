c37d160c887095cea35151b9cd51c88b53e0b830
Sebastian Poblete <pseba9006@gmail.com>
2024-10-26 01:03:31 -0300

    creacion form reserva pt1

--- src/Components/Reservation.jsx
+++ src/Components/Reservation.jsx
@@ -1,12 +1,42 @@
 // Home.jsx
 import React from 'react';
+import horas from '../const/horas';
 
 function Reservation() {
   return (
     <div>
-      <h1>Pagina de reservas</h1>
-      <p>Descubre nuestros sabores únicos y tradicionales</p>
-      {/* Agrega más contenido y estilo aquí */}
+      <form>
+        <h1>¡Reserva ahora!</h1>
+        <p>Nombre: </p>
+        <p>correo: </p>
+        <p>Telefono: </p>
+
+        <label htmlFor="grupo">Tamaño del grupo:</label>
+        <select name="grupo" id="grupo" required>
+          <option value="" disabled selected>Seleccione el tamaño del grupo</option>
+          <option value="1">1 persona</option>
+          <option value="2">2 personas</option>
+          <option value="3">3 personas</option>
+          <option value="4">4 personas</option>
+          <option value="5">5 personas</option>
+          <option value="6">6 personas</option>
+        </select>
+
+        <label htmlFor="fecha">Fecha:</label>
+        <input type="date" id='fecha' name='fecha' required/>
+
+        <label htmlFor="horario">Horario:</label>
+        <select name="horario" id="horario" required>
+          {horas.map(horario =>(
+            <option key={horario.value} value={horario.label}>
+              {horario.label}
+            </option>
+          ))}
+        </select>
+        
+
+
+      </form>
     </div>
   );
 }
---
+++ src/const/horas.jsx
@@ -0,0 +1,11 @@
+//arreglo que contiene las horas para reservas mesas
+const horas = [
+    {value:'12:00', label:'12:00'},
+    {value:'13:30', label:'13:30'},
+    {value:'15:00', label:'15:00'},
+    {value:'16:30', label:'16:30'},
+    {value:'18:00', label:'18:00'},
+    
+  ]
+  
+  export default horas;
